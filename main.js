1a3dc253052b68179ef544fea849b4f818673244
Andraž Verdnik <grillbottoms@Andrazs-MacBook-Pro.local>
2023-09-27 20:05:11 +0200

    add a check for keyboard presses and change the code for animating on click

--- main.js
+++ main.js
@@ -21,20 +21,90 @@ const equalButton = document.getElementById("equal")
 
 //Function definitions
 
+function checkForKeyPress (){
+    window.onkeydown = (keyPressed)=>{
+        switch(keyPressed.key){
+            case "0":
+                console.log("0")
+                animateG(zeroButton)
+                break
+            case "1":
+                console.log("1")
+                animateG(oneButton)
+                break
+            case "2":
+                console.log("2")
+                animateG(twoButton)
+                break
+            case "3":
+                console.log("3")
+                animateG(threeButton)
+                break
+            case "4":
+                console.log("4")
+                animateG(fourButton)
+                break
+            case "5":
+                console.log("5")
+                animateG(fiveButton)
+                break
+            case "6":
+                console.log("6")
+                animateG(sixButton)
+                break
+            case "7":
+                console.log("7")
+                animateG(sevenButton)
+                break
+            case "8":
+                console.log("8")
+                animateG(eightButton)
+                break
+            case "9":
+                console.log("9")
+                animateG(nineButton)
+                break
+            case "Backspace":
+                console.log("delete")
+                animateG(ACButton)
+                break
+            case "/":
+                console.log("/")
+                animateO(divisionButton)
+                break
+            case "x":
+                console.log("x")
+                animateO(multiplicationButton)
+                break
+            case "-":
+                console.log("-")
+                animateO(reductionButton)
+                break
+            case "=":
+                console.log("+")
+                animateO(additionButton)
+                break
+            case "Enter":
+                console.log("=")
+                animateO(equalButton)
+                break
+            }
+    }
+}
+
 function animateG(element){
-    element.addEventListener("click", ()=>{
-        element.classList.remove("gButton")
+    element.classList.remove("gButton")
         element.classList.add("pgButton")
         console.log(element)
     setTimeout(()=>{
         element.classList.remove("pgButton")
         element.classList.add("gButton")
     }, 100)
-    })
 }
 
+
+
 function animateO(element){
-    element.addEventListener("click", ()=>{
         element.classList.remove("orangeButton")
         element.classList.add("pgButton")
         console.log(element)
@@ -42,26 +112,63 @@ function animateO(element){
         element.classList.remove("pgButton")
         element.classList.add("orangeButton")
     }, 100)
+}
+
+
+function checkForClick (){
+    zeroButton.addEventListener("click", ()=>{
+        animateG(zeroButton)
+    })
+    oneButton.addEventListener("click", ()=>{
+        animateG(oneButton)
+    })
+    twoButton.addEventListener("click", ()=>{
+        animateG(twoButton)
+    })
+    threeButton.addEventListener("click", ()=>{
+        animateG(threeButton)
+    })
+    fourButton.addEventListener("click", ()=>{
+        animateG(fourButton)
+    })
+    fiveButton.addEventListener("click", ()=>{
+        animateG(fiveButton)
+    })
+    sixButton.addEventListener("click", ()=>{
+        animateG(sixButton)
+    })
+    sevenButton.addEventListener("click", ()=>{
+        animateG(sevenButton)
+    })
+    eightButton.addEventListener("click", ()=>{
+        animateG(eightButton)
+    })
+    nineButton.addEventListener("click", ()=>{
+        animateG(nineButton)
+    })
+    ACButton.addEventListener("click", ()=>{
+        animateG(ACButton)
+    })
+    divisionButton.addEventListener("click", ()=>{
+        animateO(divisionButton)
+    })
+    multiplicationButton.addEventListener("click", ()=>{
+        animateO(multiplicationButton)
+    })
+    additionButton.addEventListener("click", ()=>{
+        animateO(additionButton)
+    })
+    reductionButton.addEventListener("click", ()=>{
+        animateO(reductionButton)
+    })
+    equalButton.addEventListener("click", ()=>{
+        animateO(equalButton)
     })
 }
 
 function animateButtons(){
-animateO(equalButton)
-animateO(multiplicationButton)
-animateO(reductionButton)
-animateO(additionButton)
-animateO(divisionButton)
-animateG(zeroButton)
-animateG(oneButton)
-animateG(twoButton)
-animateG(threeButton)
-animateG(fourButton)
-animateG(fiveButton)
-animateG(sixButton)
-animateG(sevenButton)
-animateG(eightButton)
-animateG(nineButton)
-animateG(ACButton)
+    checkForClick()
+    checkForKeyPress()
 }
 
 
@@ -69,4 +176,4 @@ animateG(ACButton)
 //Function running
 
 animateButtons()
-
+checkForKeyPress()
